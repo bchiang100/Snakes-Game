9704abc4b4786f4ebf8169b61eb6a6fb2f6bbfd0
Bryan Chiang <90701819+bchiang100@users.noreply.github.com>
2024-06-25 09:16:26 -0400

    Update bcd_adder.sv

--- source/bcd_adder.sv
+++ source/bcd_adder.sv
@@ -7,12 +7,12 @@ module bcd_adder (
     logic [4:0] Sum_temp;    // Temporary 5-bit sum including carry
 
     // Calculate the sum with carry-in
-    assign Sum_temp = A + B + Cin;
+    assign Sum_temp = A + B + {4'b0, Cin};
 
     // Check if the sum exceeds BCD limit (9)
     always_comb begin
         if (Sum_temp > 9) begin
-            Sum = Sum_temp - 10;  // Adjust sum to BCD
+            Sum = Sum_temp[3:0] - 4'd10;  // Adjust sum to BCD
             Cout_bcd = 1;         // Set BCD carry
         end else begin
             Sum = Sum_temp[3:0];  // Keep sum as is
