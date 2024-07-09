d1f8260b20a01c108f1120356310f27334a4bfdd
Bryan Chiang <chian116@ee65lnx34.ecn.purdue.edu>
2024-07-09 09:58:06 -0400

    pushing

--- source/tb_sound_generator.sv
+++ source/tb_sound_generator.sv
@@ -134,7 +134,7 @@ module tb_sound_generator ();
          tb_goodColl = 1'b1;
          #(CLK_PERIOD * 5);
          tb_goodColl = 1'b0;
-         #(CLK_PERIOD * 5000); // allow for some delay
+         #(CLK_PERIOD * 900000); // allow for some delay
          
 
         #(CLK_PERIOD * 50);
@@ -142,13 +142,13 @@ module tb_sound_generator ();
          tb_badColl = 1'b1;
          #(CLK_PERIOD * 5);
          tb_badColl = 1'b0;
-         #(CLK_PERIOD * 5000); // allow for some delay
+         #(CLK_PERIOD * 900000); // allow for some delay
          
 
         #(CLK_PERIOD * 50);
 
          tb_direction = 4'b0001;
-         #(CLK_PERIOD * 5000);
+         #(CLK_PERIOD * 900000);
          tb_direction = 4'b0000;
          #(CLK_PERIOD * 100); // allow for some delay
          
